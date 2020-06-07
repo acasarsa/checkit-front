a8d801aca471952e4b7c3d4a9973a64e281b60ac
github username <github email address>
2020-06-06 21:31:32 -0400

    added types to reducer

--- src/reducer.js
+++ src/reducer.js
@@ -7,6 +7,26 @@ let initialState = {
 
 export const reducer = (prevState = initialState, action) => {
     switch (action.type) {
+        case 'CREATE_LIST':
+            return { ...prevState }
+        
+        case 'UPDATE_LIST':
+            return { ...prevState }
+        
+        case 'DELETE_LIST':
+            return { ...prevState }
+        
+        case 'FETCH_LISTS':
+            return { ...prevState }
+        
+        case 'CREATE_TASK':
+            return { ...prevState }
+        
+        case 'UPDATE_TASK':
+            return { ...prevState }
+        
+        case 'DELETE_TASK':
+            return { ...prevState }
         
         default:
             return { ...prevState }
